c2042b7d3b40854f2645ecc4825b34760c5a2b36
Valeriia Pervushyna <v.pervushyna@gmail.com>
2022-11-23 11:44:30 +0400

    Add the constants for the backtests

---
+++ modules/constants/__init__.py
@@ -0,0 +1,7 @@
+"""
+Initialize the module
+"""
+
+from modules.constants.ethmaxi_const import PARAMS
+from modules.constants.pmusdcv2_const import PARAMS
+from modules.constants.polygains_const import PARAMS
