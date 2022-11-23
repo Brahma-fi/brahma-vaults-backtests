8e8de31a13c39db4b9e9ca8b0a2c753bb9f54e10
Valeriia Pervushyna <v.pervushyna@gmail.com>
2022-11-23 17:11:38 +0400

    Deleted the redundant submodule

--- modules/backtesting/protected_moonshot/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Protrcted moonshot init
-"""
-
-from modules.backtesting.protected_moonshot.option import run_option_backtest
-from modules.backtesting.protected_moonshot.perp import run_perp_backtest
